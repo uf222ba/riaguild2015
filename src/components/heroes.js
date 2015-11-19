c8d44ed7707125dfc0e5a10471b00537d02b0b7f
Moomba <joe@rm-audio.com>
2015-11-19 18:09:11 +0100

    Added a "silver" tier of Heroes to the Heroes section. Made the original Heroes "gold" Heroes

--- src/components/heroes.js
+++ src/components/heroes.js
@@ -17,20 +17,28 @@ var Heroes = React.createClass({
 					</thead>
 					<tbody>
 						<tr>
-							<td>Our <strong>most prolific bloggers</strong> <br/> have written {heroes.blogposts[0]} posts:</td>
-							<td>{ _.map(heroes.blogposts[1],function(id){ return <Badge key={id} id={id} />}) }</td>
+							<td>Our <strong>most prolific bloggers</strong> <br/> have written <font className="gold-text">{heroes.blogposts[0]} posts</font>:
+							<br />(Runners up have written <font className="silver-text">{heroes.blogposts[2]}</font>)</td>
+							<td>{ _.map(heroes.blogposts[1],function(id){ return <Badge honour="gold" key={id} id={id} />}) }
+								{ _.map(heroes.blogposts[3],function(id){ return <Badge honour="silver" key={id} id={id} />}) }</td>
 						</tr>
 						<tr>
-							<td>Our <strong>most helpful members</strong> <br/> have made {heroes.pullrequests[0]} pull requests:</td>
-							<td>{ _.map(heroes.pullrequests[1],function(id){ return <Badge key={id} id={id} />}) }</td>
+							<td>Our <strong>most helpful members</strong> <br/> have made <font className="gold-text">{heroes.pullrequests[0]} pull requests</font>:
+							<br />(Runners up have made <font className="silver-text">{heroes.pullrequests[2]}</font>)</td>
+							<td>{ _.map(heroes.pullrequests[1],function(id){ return <Badge honour="gold" key={id} id={id} />}) }
+								{ _.map(heroes.pullrequests[3],function(id){ return <Badge honour="silver" key={id} id={id} />}) }</td>
 						</tr>
 						<tr>
-							<td>Our <strong>wisest members</strong> have <br/> written {heroes.sageadvice[0]} sage advice:</td>
-							<td>{ _.map(heroes.sageadvice[1],function(id){ return <Badge key={id} id={id} />}) }</td>
+							<td>Our <strong>wisest members</strong> have <br/> written <font className="gold-text">{heroes.sageadvice[0]} sage advice</font>:
+							<br />(Runners up have written <font className="silver-text">{heroes.sageadvice[2]}</font>)</td>
+							<td>{ _.map(heroes.sageadvice[1],function(id){ return <Badge honour="gold" key={id} id={id} />}) }
+								{ _.map(heroes.sageadvice[3],function(id){ return <Badge honour="silver" key={id} id={id} />}) }</td>
 						</tr>
 						<tr>
-							<td>Our <strong>most generous coders</strong> have <br/> written {heroes.snippets[0]} code snippets:</td>
-							<td>{ _.map(heroes.snippets[1],function(id){ return <Badge key={id} id={id} />}) }</td>
+							<td>Our <strong>most generous coders</strong> have <br/> written <font className="gold-text">{heroes.snippets[0]} code snippets</font>:
+							<br />(Runners up have written <font className="silver-text">{heroes.snippets[2]}</font>)</td>
+							<td>{ _.map(heroes.snippets[1],function(id){ return <Badge honour="gold" key={id} id={id} />}) }
+								{ _.map(heroes.snippets[3],function(id){ return <Badge honour="silver" key={id} id={id} />}) }</td>
 						</tr>
 					</tbody>
 				</table>
